313b6016ac817e1a0704ab445e4c959320f8f7b1
Abhishek Singh <singhabhi9865@gmail.com>
2025-09-13 18:09:57 +0530

    4

--- Design Patterns/SimpleFactory.cpp
+++ Design Patterns/SimpleFactory.cpp
@@ -1,8 +1,5 @@
-#include <iostream>
 #include <string>
-
-//product
-//creator
+#include <iostream>
 
 enum class NotificationType {
     SMS,
@@ -37,5 +34,32 @@ public:
     void notify(const std::string& message) override {
         // Implementation for sending WhatsApp notification
         std::cout << "WhatsApp Notification: " << message << std::endl;
+    }
+};
+
+class NotificationFactory {
+public:
+    static Notification* createNotification(NotificationType type) {
+        switch (type) {
+            case NotificationType::SMS:
+                return new SMSNotification();
+            case NotificationType::Email:
+                return new EmailNotification();
+            case NotificationType::WhatsApp:
+                return new whatsappNotification();
+            default:
+                return nullptr;
+        }
+    }
+};
+
+class NotificationManager {
+    public:
+        void sendNotification(NotificationType type, const std::string& msg){
+            Notification* notification = NotificationFactory::createNotification(type);
+            if(notification){
+                notification->notify(msg);
+                delete notification;
+        }   
     }
 };
